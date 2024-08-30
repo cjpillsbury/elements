50db9efc1684e45ab4efe2b5b0e5d242d205b9f7
Christian Pillsbury <cpillsbury@mux.com>
2024-08-30 11:53:36 -0700

    build: Upgrade to latest TS version. update tsconfig to moduleResolution bundle for packages. Resolve TS 5.x errors and syntax changes.

--- packages/playback-core/src/types.ts
+++ packages/playback-core/src/types.ts
@@ -1,5 +1,5 @@
 /* eslint @typescript-eslint/triple-slash-reference: "off" */
-/// <reference path="../../../node_modules/mux-embed/dist/types/mux-embed.d.ts" />
+/// <reference path="../../../node_modules/mux-embed/dist/types/mux-embed.d.ts" preserve="true" />
 import type { Options } from 'mux-embed';
 import type { MediaError } from './errors';
 import type { HlsConfig } from 'hls.js';
